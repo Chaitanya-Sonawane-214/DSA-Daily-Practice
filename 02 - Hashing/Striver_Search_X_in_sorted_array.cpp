33f9a752e71d4242ded1f699ef50e4f6037a9244
Chaitanya Sonawane <chaitanyasonawane201@gmail.com>
2026-08-05 08:33:03 +0530

    Striver - Search X in sorted array

---
+++ 02 - Hashing/Striver_Search_X_in_sorted_array.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else if(nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else 
+            {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main() 
+{
+    Solution sol;
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    
+    int result = sol.search(nums, target);
+    cout << result << endl;
+    
+    return 0;
+}
