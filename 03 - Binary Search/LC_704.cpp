e10657852b5ea5f598da9e8f0cacd2844a61dd54
Chaitanya Sonawane <chaitanyasonawane201@gmail.com>
2026-08-06 11:32:11 +0530

    Leetcode - #704

---
+++ 03 - Binary Search/LC_704.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else if(nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }    
+        return -1;
+    }
+};
+
+int main() 
+{
+    Solution sol;
+    vector<int> nums = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    
+    int result = sol.search(nums, target);
+    cout << result << endl;
+    
+    return 0;
+}
